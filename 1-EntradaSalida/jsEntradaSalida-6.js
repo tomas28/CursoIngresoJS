4ed088b60e0d1dc464b82916d4c1b9955f2f2178
Patricio Tomas Garcia <47353690+tomas28@users.noreply.github.com>
2019-02-05 15:36:57 -0300

    ejercicio 6 finalizado

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,12 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var primerNumero = parseInt(primerNumero =numeroUno.value);
+var segundoNumero = parseInt(segundoNumero = numeroDos.value);
+  
+
+alert("La suma de los nuneros da: " + (primerNumero + segundoNumero));
+
 
 }
 
