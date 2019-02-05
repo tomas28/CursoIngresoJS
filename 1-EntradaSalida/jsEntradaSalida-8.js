660408aa68f7fd8b7496c2839729b3cf3e36d926
Patricio Tomas Garcia <47353690+tomas28@users.noreply.github.com>
2019-02-05 15:53:01 -0300

    ejercicio 8 termiandp

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,9 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+var dividendo = parseInt(dividendo =numeroDividendo.value);
+var divisor = parseInt(divisor = numeroDivisor.value);
+  
+
+alert("el resto es: " + (dividendo % divisor));	
 }
