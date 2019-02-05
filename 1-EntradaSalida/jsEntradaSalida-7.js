d510885b724e5c5f3b8c249eff7276d22b432483
Patricio Tomas Garcia <47353690+tomas28@users.noreply.github.com>
2019-02-05 15:45:51 -0300

    ejercicio 7 terminado
    practicando

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,39 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+var primerNumero = parseInt(primerNumero =numeroUno.value);
+var segundoNumero = parseInt(segundoNumero = numeroDos.value);
+  
+
+alert("La suma de los nuneros da: " + (primerNumero + segundoNumero));
+
 }
 
 function restar()
 {
-	
+var primerNumero = parseInt(primerNumero =numeroUno.value);
+var segundoNumero = parseInt(segundoNumero = numeroDos.value);
+  
+
+alert("La resta de los nuneros da: " + (primerNumero - segundoNumero));	
+
 }
 
 function multiplicar()
 { 
-	
+var primerNumero = parseInt(primerNumero =numeroUno.value);
+var segundoNumero = parseInt(segundoNumero = numeroDos.value);
+  
+
+alert("La multiplicacion de los nuneros da: " + (primerNumero * segundoNumero));	
 }
 
 function dividir()
 {
-	
+var primerNumero = parseInt(primerNumero =numeroUno.value);
+var segundoNumero = parseInt(segundoNumero = numeroDos.value);
+  
+
+alert("La divicion de los nuneros da: " + (primerNumero / segundoNumero));	
 }
 
