518437ca4cc3884f8aba3e78f49b3e5ee70457dc
Patricio Tomas Garcia <47353690+tomas28@users.noreply.github.com>
2019-02-05 16:21:23 -0300

    ejercicio 9 terminado

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,10 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var aumento
+var importe = parseInt(importe =sueldo.value);
+ 
+aumento = (((importe * 10) /100) + importe);
 
-	
+resultado.value = aumento
 }
