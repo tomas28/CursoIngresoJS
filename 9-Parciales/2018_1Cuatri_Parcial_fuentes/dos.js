7f12e39145eabc37330dad1190f4394ab314f21e
tomas28 <patricio_garcia28@hotmail.com>
2019-02-06 11:14:34 -0300

    segundo ejercicio del parcial terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,8 @@
 function mostrar()
 {
-  
+  var nombre;
+  var localidad;
+  nombre = elNombre.value;
+  localidad = laLocalidad.value;
+  alert("usted se llama "+nombre+" y usted vive en la localidad "+localidad);
 }
