83b490572e785dc1175baf62a82189e988a39b92
tomas28 <patricio_garcia28@hotmail.com>
2019-02-06 10:51:28 -0300

    primer ejercicio del parcial completado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,11 @@
 
 function mostrar()
 {
-
+	var ancho;
+	var largo;
+	ancho = prompt("porfavor ingrese el ancho del rectangulo");
+	ancho = parseInt(ancho);
+	largo=prompt("porfavor ingrese el largo del rectangulo");
+	largo=parseInt(largo);
+	alert("el ancho del rectangulo es: "+ancho+"el largo del rectangulo es: "+largo);
 }
