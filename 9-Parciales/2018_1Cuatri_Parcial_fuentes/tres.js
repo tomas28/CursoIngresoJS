0ef67758a7d9d760bb5374a3a046b9a88332905e
tomas28 <patricio_garcia28@hotmail.com>
2019-02-06 11:24:02 -0300

    tercer ejercicio del parcial incompleto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
-
+var precio;
+var porcentaje;
+var precioFinal;
+precio=prompt("ingrese el precio que corresponde ");
+precio=parseInt(precio);
+porcentaje=prompt("ingrese el porcentaje de descuento que corresponda ");
+porcentaje=parseInt(porcentaje);
+precioFinal=((precio*porcentaje)/100)+precio;
+precioFinal=elPrecioFinal.value;
 }
