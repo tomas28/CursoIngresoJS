41f3fd570d2e23f90d6c908d7d161626a7dded0c
alumno <alumno>
2019-02-13 09:29:29 -0300

    segundo ejercicio del parcial terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,8 +1,19 @@
 function mostrar()
 {
-  var nombre;
-  var localidad;
-  nombre = elNombre.value;
-  localidad = laLocalidad.value;
-  alert("usted se llama "+nombre+" y usted vive en la localidad "+localidad);
+	var precioEfectivo;
+ 	var aumento;
+ 	var resultado;
+
+ 	precioEfectivo=prompt("ingrese el precio del producto a pagar");
+ 	precioEfectivo=parseInt(precioEfectivo);
+
+  	aumento=(precioEfectivo*10)/100;
+
+  	resultado=precioEfectivo+aumento;
+
+ 	alert("el precio del producto en efectivo es: $"+precioEfectivo+", con tarjeta tiene un recargo del 10% que seria : $"+aumento+" de recargo, costandole final $"+resultado);
+
+
+
+
 }
