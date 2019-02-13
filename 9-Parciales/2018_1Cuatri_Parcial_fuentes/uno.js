e8756a7380bd4d91d01082f86a7ab685575f4432
alumno <alumno>
2019-02-13 09:11:32 -0300

    primer ejercicio del parcial terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,17 +1,24 @@
 
 function mostrar()
 {
-var ancho;
-var largo;
-var perimetro;
+	var nombre1;
+	var nombre2;
+	var edad1;
+	var edad2;
+	var resultado;
 
-ancho=prompt("ingrese el ancho del rectagulo");
-ancho=parseInt(ancho);
+	nombre1=prompt("ingrese su nombre ");
+	nombre2=prompt("ingrese su nombre");
 
-largo=prompt("ingrese el largo del rectangulo");
-largo=parseInt(largo);
+	edad1=prompt("ingrese la edad del primer nombre que ingreso anteriormente" );
+	edad1=parseInt(edad1);
 
-perimetro=(ancho+largo)*2
-alert("el perimetro del rectangulo es: "+perimetro);
+	edad2=prompt("ingrese la edad del segundo nombre que ingreso anteriormente");
+	edad2=parseInt(edad2);
+	
+	resultado= edad1+edad2;
+
+
+	alert("ustedes son: "+nombre1+" y "+nombre2+", sus edades son:  "+edad1+" y "+edad2+", y la suma de sus edades son: "+resultado);
 	
 }
